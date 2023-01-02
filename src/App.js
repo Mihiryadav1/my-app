c28d052867c8ccbc275e222a9409622f2ae792e8
Mihiryadav1 <yadavmihir30@gmail.com>
2023-01-02 13:51:53 +0530

    Fianl Changes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,22 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Formjs from './components/Formsjs';
+import React, { useState } from 'react';
 function App() {
+  const [mode, setmode] = useState('light');
+  const togglemode = () => {
+    if (mode === 'light') {
+      setmode('dark');
+    }
+    else {
+      setmode('light');
+    }
+  }
   return (
     <>
-      <Navbar title="TextUtils" aboutText="AboutUs" />
+      <Navbar title="TextUtils" aboutText="AboutUs" mode={mode} togglemode={togglemode} />
       <div className="container">
-      <Formjs/>
+        <Formjs />
         {/* <TextForm heading="This is textarea" /> */}
       </div>
     </>
--- src/components/Formsjs.js
+++ src/components/Formsjs.js
@@ -17,20 +17,28 @@ export default function Formjs(props) {
     }
     const handleClear = (event) => {
         console.log("Cleared Text");
-        let newText="";
+        let newText = "";
         settext(newText);
 
     }
+    const handleCopy = (event) => {
+       
+        let textbox=document.getElementById("box1");
+        textbox.select();
+        navigator.clipboard.writeText(textbox.value);
+    };
+
     return (
         <>
             <div className="container my-4">
                 <div className="mb-3">
                     {/* <label for="exampleFormControlTextarea1" className="form-label">Example textarea</label> */}
                     <h1>Enter  your text here</h1>
-                    <textarea className="form-control" value={text} onChange={handleOnChange} id="exampleFormControlTextarea1" rows="8"></textarea>
+                    <textarea className="form-control" value={text} onChange={handleOnChange} id="box1" rows="8"></textarea>
                 </div>
                 <button className="btn btn-primary" onClick={handleUpClick}>Click to change</button>
-                <button className="btn btn-primary" onClick={handleClear}>Clear Text</button>
+                <button className="btn btn-primary mx-3" onClick={handleClear}>Clear Text</button>
+                <button className="btn btn-primary mx-3" onClick={handleCopy}>Copy Text</button>
             </div>
             <div className="number">
                 <h1>Text summary </h1>
